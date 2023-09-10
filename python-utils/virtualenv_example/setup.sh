1b5bfc18cf7fe2f74fa8ed56c55cd4b1eeb61c48
Brent Brewington <brent.brewington@gmail.com>
2023-09-10 11:06:19 -0400

    Update setup.sh

--- python-utils/virtualenv_example/setup.sh
+++ python-utils/virtualenv_example/setup.sh
@@ -1,23 +1,44 @@
-# Make sure .venv is in .gitignore
-if grep -q '^.venv$' .gitignore; then
-  echo "Virtual environment already in .gitignore"
-else
-  echo '.venv' >> .gitignore
+# Set working directory to /dbt
+cd $(git rev-parse --show-toplevel)/dbt;
+
+# Create .gitignore if doesn't exist
+if [[ ! -e .gitignore ]]; then
+    touch .gitignore;
 fi
 
-# Set platform-specific subdir (handles if running this on Windows)
+# Deactivate virtual environment if currently active
+if [[ "$VIRTUAL_ENV" != "" ]]; then
+    echo 'deactivating Virtual Env';
+    deactivate;
+fi
+
+# Set variables
+
+#   General variables
+VENV_NAME=.venv_dbt;
+
+#   Set platform-specific subdir (handles if running this on Windows)
 if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
-    VENV_SUBDIR="Scripts"
+    VENV_SUBDIR="Scripts";
+    PYTHON_EXEC=python;
 else
-    VENV_SUBDIR="bin"
+    VENV_SUBDIR="bin";
+    PYTHON_EXEC=python3;
+fi
+
+# Make sure VENV_NAME is in .gitignore
+if grep -q "^$VENV_NAME" .gitignore; then
+    echo "Virtual environment already in .gitignore";
+else
+    echo $VENV_NAME >> .gitignore;
 fi
 
 # Create virtual environment & activate it
-python3 -m venv .venv
-source .venv/$VENV_SUBDIR/activate
-python3 -m pip install --upgrade pip
-pip install -r requirements.txt
+$PYTHON_EXEC -m venv $VENV_NAME;
+source "$VENV_NAME/$VENV_SUBDIR/activate";
+$PYTHON_EXEC -m pip install --upgrade pip;
+pip install -r requirements_dbt.txt;
 
-# If you need to run this virtual environment in a Jupyter Notebook, uncomment these lines
+# 6. If you need to run this virtual environment in a Jupyter Notebook, uncomment these lines
 # pip install ipykernel
-# python3 -m ipykernel install --user --name ADD_NAME_HERE
+# $PYTHON_EXEC -m ipykernel install --user --name ADD_NAME_HERE
